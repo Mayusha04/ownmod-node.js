dd124c1314d070bde7028bfa361f024f6dfc9f09
Mayusha04 <mayushachowdary95@gmail.com>
2022-01-05 22:28:06 +0530

    own mod

---
+++ index.js
@@ -0,0 +1,6 @@
+var calculator = require('./calc.js');
+var a=12 , b=6;
+console.log("Addition of a and b is" + calculator.add(a,b));
+console.log("Subtraction of a and b is" + calculator.sub(a,b));
+console.log("Multiplication of a and b is" + calculator.mul(a,b));
+console.log("Division of a and b is" + calculator.div(a,b));
